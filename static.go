569ae7ea0a0932c4d2c417813a03b689a02f8582
emiliopalmerini <emilio.palmerini@gmail.com>
2026-01-04 13:45:30 +0100

    style: fix gofmt formatting in static.go
    Apply gofmt to fix comment formatting detected by CI.

--- static.go
+++ static.go
@@ -6,8 +6,9 @@ import "embed"
 // These files can be served directly via http.FileServer.
 //
 // Usage:
-//   import designsystem "github.com/emiliopalmerini/due-draghi-design-system"
-//   http.Handle("/design-system/", http.StripPrefix("/design-system/", http.FileServer(http.FS(designsystem.StaticFiles))))
+//
+//	import designsystem "github.com/emiliopalmerini/due-draghi-design-system"
+//	http.Handle("/design-system/", http.StripPrefix("/design-system/", http.FileServer(http.FS(designsystem.StaticFiles))))
 //
 //go:embed css/*.css js/*.js
 var StaticFiles embed.FS
